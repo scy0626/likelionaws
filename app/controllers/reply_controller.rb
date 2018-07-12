1638d374ccd146d72c7577577069342558adbc2a
ChaeYoung Son <scy0626@likelion.org>
2018-07-12 09:58:47 +0000

    heroku

---
+++ app/controllers/reply_controller.rb
@@ -0,0 +1,22 @@
+class ReplyController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @reply = Reply.new
+    @reply.content = params[:content]
+    @reply.post_id = params[:post_id]
+    @reply.user_id = current_user.id
+    @reply.save
+    
+    redirect_to :back
+  end
+
+  def destroy
+    @reply = Reply.find(params[:id])
+    
+    if current_user.id == @reply.user_id
+      @reply.destroy
+    end
+     redirect_to :back
+    
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+#User.create(username:"111", email: "1@1", password: "111111", encrypted_password: "111111")
+#Post.create(title: "안녕하세요", content: "반갑습니다.", user_id: 1)
