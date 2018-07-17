e1db8af1a251379e5d20380e8f904d30690611df
ChaeYoung Son <scy0626@likelion.org>
2018-07-17 09:45:25 +0000

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #User.create(username:"111", email: "1@1", password: "111111", encrypted_password: "111111")
 #Post.create(title: "안녕하세요", content: "반갑습니다.", user_id: 1)
+(1..3).each do |i|
+    User.create(username: i , email: "#{i}@#{i}.com" , password: "121212" , password_confirmation: "121212")
+    (1..3).each do |j|
+        Post.create(title: "#{i}가 #{j}번째 글씀" , content: "#{i}가 쓴#{j}번째 글임", user_id: i)
+    end
+end
